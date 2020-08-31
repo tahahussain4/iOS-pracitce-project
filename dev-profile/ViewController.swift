1f916835f5ee523f961c98faac5ccda8790fa371
Taha Hussain <tahahussain1994@gmail.com>
2020-08-31 10:16:00 -0400

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by taha hussain on 2020-08-31.
+//  Copyright © 2020 taha hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ dev-profileTests/dev_profileTests.swift
@@ -0,0 +1,34 @@
+//
+//  dev_profileTests.swift
+//  dev-profileTests
+//
+//  Created by taha hussain on 2020-08-31.
+//  Copyright © 2020 taha hussain. All rights reserved.
+//
+
+import XCTest
+@testable import dev_profile
+
+class dev_profileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
